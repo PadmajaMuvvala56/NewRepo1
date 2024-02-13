d3ed806122bc7bc91099c51b3adaf4792f3ba1eb
ajayk <ajayk@PADMAJAGAJAVALLI>
2024-02-13 13:06:35 +0530

    next commit of TestNG_demo2.java

--- Selenium4ProjTest/src/test/java/test/TestNG_demo2.java
+++ Selenium4ProjTest/src/test/java/test/TestNG_demo2.java
@@ -1,27 +1,38 @@
 package test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNG_demo2 {
 
 	WebDriver driver;
 
+	@Parameters("browserName")
 	@BeforeTest
-	public void setUpTest() {
-		driver = new ChromeDriver();
+	public void setUpTest(@Optional String browserName) throws Exception {
+		System.out.println("browser name is : " + browserName);
+
+		if (browserName.equalsIgnoreCase("chrome"))
+			driver = new ChromeDriver();
+		// System.out.println("driver");
+		else if (browserName.equalsIgnoreCase("firefox"))
+			driver = new FirefoxDriver();
+		else if (browserName.equalsIgnoreCase("edge"))
+			driver = new EdgeDriver();
 	}
 
 	@Test
 	public void googleSearch2() throws InterruptedException {
-
+		System.out.println("inside googlesearch2");
 		// System.setProperty(webdriver.chrome.driver,"..path of driver exe...")
 
 		// not needed above or WebDriverManager also for latest build of Se4 bcos of
@@ -31,9 +42,9 @@ public class TestNG_demo2 {
 		// WebDriverManager.chromedriver.setup();
 		// WebDriverManager.chromedriver.driverVersion("92.0").setup();
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+		// driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
 		driver.get("https://google.com");
 
@@ -63,7 +74,15 @@ public class TestNG_demo2 {
 
 	@AfterTest
 	public void tearDownTest() {
-		driver.close();
-		driver.quit();
+		try {
+			driver.close();
+			// driver.quit();
+			// commented bcos quit should not be used when doing multi browser testing or
+			// parallel testing which closes the sessions running and gives exception
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+
+		}
 	}
 }
